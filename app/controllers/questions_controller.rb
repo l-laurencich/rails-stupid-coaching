7cd4e01fcf60ececd9d76c3c8c10718d159f0663
Lucas Laurencich <lucas.maximiliano89@gmail.com>
2021-05-17 12:48:28 +0200

    as far as i got

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,16 @@
+class QuestionsController < ApplicationController
+  def ask
+  end
+
+  def answer
+    answers = ["Great", "Silly question, get dressed and go to work!", "I don't care, get dressed and go to work!"]
+
+    if params[:question] == 'I am going to work right now!'
+      @answer = answers[0]
+    elsif params[:question].include? '?'
+      @answer = answers[1]
+    else
+      @answer = answer[2]
+    end
+  end
+end
